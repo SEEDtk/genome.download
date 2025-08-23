c54f87dee4dd84ee2286b35ed8cd19161acbfd00
Bruce Parrello <bparrello@figresearch.com>
2025-08-23 09:11:52 -0500

    Added synchronization support to PATRIC downloader.

--- src/main/java/org/theseed/genome/download/PatricProcessor.java
+++ src/main/java/org/theseed/genome/download/PatricProcessor.java
@@ -47,6 +47,9 @@ import org.theseed.subsystems.core.SubsystemRuleProjector;
  * --missing	only download genomes not already present
  * --projector	if specified, the name of a subsystem projector file for computing the subsystems
  * --level		detail level to download; the default is FULL
+ * --clean      remove any genomes in the output directory not in the input list
+ * --sync       synchronize the target to the input list (shorthand for --missing and --clean)
+ * --messages   number of seconds between status messages (default 5)
  *
  * @author Bruce Parrello
  *
@@ -87,9 +90,17 @@ public class PatricProcessor extends BaseProcessor {
     @Option(name = "--clear", usage = "erase output directory before starting")
     private boolean clearOutput;
 
+    /** TRUE to remove genomes not in the input list */
+    @Option(name = "--clean", usage = "remove any genomes not in the input list")
+    private boolean cleanOutput;
+
     /** TRUE to only download genomes not already in the directory */
     @Option(name = "--missing", usage = "only download new genomes")
     private boolean missingOnly;
+
+    /** TRUE to synchronize the target to the input list */
+    @Option(name = "--sync", usage = "synchronize the target to the input list (shorthand for --missing and --clean)")
+    private boolean synchronize;
 
     /** subsystem projector file name */
     @Option(name = "--projector", metaVar = "projector.ser", usage = "optional projector file for computing subsystems")
@@ -98,6 +109,10 @@ public class PatricProcessor extends BaseProcessor {
     /** detail level to download */
     @Option(name = "--level", usage = "detail level needed for the genome")
     private P3Genome.Details level;
+
+    /** number of seconds to wait between status messages on the log */
+    @Option(name = "--messages", metaVar = "10", usage = "number of seconds between status messages (0 = off)")
+    private int statusMessageInterval;
 
     /** type of genome target */
     @Option(name = "--target", usage = "type of genome target; the default is DIR (GTO directory)")
@@ -114,28 +129,36 @@ public class PatricProcessor extends BaseProcessor {
         this.targetType = GenomeTargetType.DIR;
         this.clearOutput = false;
         this.missingOnly = false;
+        this.synchronize = false;
         this.projectorFile = null;
         this.batchSize = 200;
+        this.cleanOutput = false;
+        this.statusMessageInterval = 5;
         this.level = P3Genome.Details.FULL;
     }
 
     @Override
     protected void validateParms() throws IOException {
+        // This implements the use of --sync as a shorthand for --missing AND --clean.
+        if (this.synchronize) {
+            this.missingOnly = true;
+            this.cleanOutput = true;
+        }
         // Validate the batch size.
         if (this.batchSize < 1)
             throw new IOException("Batch size must be at least 1.");
         log.info("Output will be to {} target {}.", this.targetType, this.outDir);
         // Validate the output directory and connect to it.
         this.genomeTarget = this.targetType.create(this.outDir, this.clearOutput);
-        // If we have the missing option set, check for existing genomes.
-        if (! this.missingOnly)
+        // If we have the missing or clean option set, check for existing genomes.
+        if (! this.missingOnly && ! this.cleanOutput)
             this.existingGenomes = Collections.emptySet();
         else try {
             this.existingGenomes = this.genomeTarget.getGenomeIDs();
         } catch (UnsupportedOperationException e) {
             // Here we have one of the single-file outputs that does not support figuring out what is
             // already in the target.
-            log.warn("The genome target type {} does not allow the --missing option.", this.targetType);
+            log.warn("The genome target type {} does not allow the --missing or --clean option.", this.targetType);
             this.existingGenomes = Collections.emptySet();
         }
         // Set up the input.
@@ -150,6 +173,7 @@ public class PatricProcessor extends BaseProcessor {
         }
         // Connect to PATRIC.
         this.p3 = new P3CursorConnection();
+        this.p3.setMessageGap(this.statusMessageInterval);
         // Set up the subsystem projector.
         this.projector = null;
         if (this.projectorFile != null) {
@@ -163,6 +187,8 @@ public class PatricProcessor extends BaseProcessor {
     @Override
     public void runCommand() throws Exception {
         try {
+            // This is used to estimate load speed.
+            long start = System.currentTimeMillis();
             // Count the genome IDs read.
             int gCount = 0;
             // Count the genomes skipped.
@@ -176,19 +202,25 @@ public class PatricProcessor extends BaseProcessor {
                 String genomeId = line.get(this.colIdx);
                 gCount++;
                 // Only proceed if we are NOT skipping this genome.
-                if (this.existingGenomes.contains(genomeId))
+                if (this.missingOnly && this.existingGenomes.contains(genomeId)) {
                     skipCount++;
-                else {
+                    // We still have to delete it from the existing-genomes list so we
+                    // don't delete it during cleanup.
+                    this.existingGenomes.remove(genomeId);
+                } else {
                     // Ensure there is room for this genome.
                     if (genomeIds.size() >= this.batchSize) {
                         // Process the current batch.
                         batchCount++;
                         log.info("Processing batch {} of {} genomes.", batchCount, genomeIds.size());
                         this.processBatch(genomeIds);
+                        log.info("PROGRESS: {} genomes downloaded at {}ms/genome.", gCount, (System.currentTimeMillis() - start) / gCount);
                         genomeIds.clear();
                     }
                     // Add this genome ID to the current batch.
                     genomeIds.add(genomeId);
+                    // Remove it from the existing-genomes list if it is there.
+                    this.existingGenomes.remove(genomeId);
                 }
             }
             // Process the residual batch (if any).
@@ -196,8 +228,22 @@ public class PatricProcessor extends BaseProcessor {
                 batchCount++;
                 log.info("Processing final batch of {} genomes.", genomeIds.size());
                 this.processBatch(genomeIds);
+                log.info("PROGRESS: {} genomes downloaded at {}ms/genome.", gCount, (System.currentTimeMillis() - start) / gCount);
             }
-            log.info("All done. {} genomes found, {} skipped, {} batches processed.", gCount, skipCount, batchCount);
+            // Now we do cleanup, if necessary.
+            int deleteCount = 0;
+            if (this.cleanOutput && ! this.existingGenomes.isEmpty()) {
+                log.info("Removing {} genomes not in input list.", this.existingGenomes.size());
+                for (String genomeId : this.existingGenomes) {
+                    try {
+                        this.genomeTarget.remove(genomeId);
+                        deleteCount++;
+                    } catch (IOException e) {
+                        log.warn("Could not delete genome {}: {}.", genomeId, e.getMessage());
+                    }
+                }
+            }
+            log.info("All done. {} genomes found, {} skipped, {} deleted, {} batches processed.", gCount, skipCount, deleteCount, batchCount);
         } finally {
             // Close the input file.
             this.inStream.close();
